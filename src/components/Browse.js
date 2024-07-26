94066ec781b0fe65564f54d19d4709e43061016d
dhruv3375 <pateldhruv2351@gmail.com>
2024-07-27 01:40:13 +0530

    Register TMDB API , Create App & Made API CALL (Movie LIst)

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from "../components/Header";
+import { API_OPTIONS } from '../utils/constant';
 
 const Browse = () =>{
+
+  const getNowplayingMovies = async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS) ;
+    const json = await data.json();
+    console.log(json.results);
+  };
+
+  // i want to call this api only once when component renders first time so we will use useeffect
+  useEffect(() => {
+    getNowplayingMovies();
+  } , [])
   return (
     <div>
       <Header/>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,11 @@
 export const logo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const userIcon = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+
+export const API_OPTIONS = 
+    {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmY2QwNjkyOGY0ZmM3NWE3ZGJkOGM0NDM4NzY3ZmY0ZCIsIm5iZiI6MTcyMjAyMzQzOS4zMzg3MDMsInN1YiI6IjY2YTNmYjBlNjIxMWUwNGJlNThkODI0NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6YRro3ca7b_gdp-O5wnAvkfaZNAXDtRo4vCVL-0tLAY'
+    }
+};
