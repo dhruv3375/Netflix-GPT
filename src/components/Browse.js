7b92c466ba202f1660c909a918dc0edb477a5907
dhruv3375 <pateldhruv2351@gmail.com>
2024-07-27 01:57:08 +0530

    dispatch the Action(Add movies to the Store)

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react'
 import Header from "../components/Header";
 import { API_OPTIONS } from '../utils/constant';
+import { useDispatch } from 'react-redux';
+import {addNowPlayingMovies} from "../utils/moviesSlice";
 
 const Browse = () =>{
+
+  const dispatch = useDispatch();
 
   const getNowplayingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS) ;
     const json = await data.json();
     console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
   };
 
   // i want to call this api only once when component renders first time so we will use useeffect
